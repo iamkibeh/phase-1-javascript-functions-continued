89518b7c5e9df18560b5d73fcf0ec4272f7f899f
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-18 11:29:47 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("play Fifa");
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork("play football"));
+
+function wrapAdjective(param1 = "*") {
+  return function (param2 = "special") {
+    return `You are ${param1 + param2+param1}!`;
+  };
+}
+const encouragingPromptFunction = wrapAdjective();
+console.log(encouragingPromptFunction("a dedicated programmer"));
